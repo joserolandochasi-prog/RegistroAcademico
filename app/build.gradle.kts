8125433394b3b0f87b566b1d82d5d4b608fc63f9
joserolandochasi-prog <joserolandochasi@gmail.com>
2026-03-05 18:54:48 -0500

    Cambio de version y corrección de compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.registroacademico"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.registroacademico"
         minSdk = 30
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
